bff7b8b08bba3cabf0f2a477617b34cbb780436a
Tyler Robbins <tyler@familyrobbins.com>
2020-08-18 12:41:40 -0700

    Removed static {} section and replaced it with direct initializations. Fixed naming of Item constants so that ObjectHolder can properly populate them. Added additional Item fields.

--- src/main/java/com/aflyingcar/warring_states/WarringStatesItems.java
+++ src/main/java/com/aflyingcar/warring_states/WarringStatesItems.java
@@ -5,11 +5,9 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @GameRegistry.ObjectHolder(WarringStatesMod.MOD_ID)
 public class WarringStatesItems {
-    public final static Item ITEM_CLAIMER_BLOCK;
-    public final static Item ITEM_WARGOAL_CLAIMER;
-
-    static {
-        ITEM_CLAIMER_BLOCK = null;
-        ITEM_WARGOAL_CLAIMER = null;
-    }
+    public final static Item CLAIMER = null;
+    public final static Item WARGOAL_CLAIMER = null;
+    public final static Item DORMANT_CLAIMER = null;
+    public final static Item FLAG_BASE = null;
+    public final static Item FLAG_POLE = null;
 }
