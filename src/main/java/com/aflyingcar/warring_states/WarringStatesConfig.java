16645b254683ccb900019fc83973afed00bc793a
Tyler Robbins <tyler@familyrobbins.com>
2020-08-22 15:18:13 -0700

    Added more configuration options.

--- src/main/java/com/aflyingcar/warring_states/WarringStatesConfig.java
+++ src/main/java/com/aflyingcar/warring_states/WarringStatesConfig.java
@@ -83,4 +83,13 @@ public class WarringStatesConfig {
 
     @Config.Comment("Minimum amount of experience required for declaring a steal chunk wargoal.")
     public static float minimumExperienceRequiredForStealingChunks = 0;
+
+    @Config.Comment("Used to calculate how much time is needed before another claim can be made. Used with claimWaitTimeMultiplier as: 'base + multiplier * numberOfClaims'")
+    public static int baseClaimWaitTime = 1;
+
+    @Config.Comment("Used to calculate how much time is needed before another claim can be made. Used with baseClaimWaitTime as: 'base + multiplier * numberOfClaims'")
+    public static int claimWaitTimeMultiplier = 30;
+
+    @Config.Comment("Number of initial claims that are free before the wait time equation takes effect.")
+    public static int numFreeClaims = 1;
 }
