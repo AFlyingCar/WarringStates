46d083d8480a8c859f1cfdf2f929196569bfac54
Tyler Robbins <tyler@familyrobbins.com>
2020-10-19 20:32:45 -0700

    Added code so that when a player logs in, logs out, or dies the appropriate method is called for every wargoal for the conflict they are currently involved in (if any).

--- src/main/java/com/aflyingcar/warring_states/handlers/PlayerEventsHandler.java
+++ src/main/java/com/aflyingcar/warring_states/handlers/PlayerEventsHandler.java
@@ -2,21 +2,30 @@ package com.aflyingcar.warring_states.handlers;
 
 import com.aflyingcar.warring_states.WarringStatesConfig;
 import com.aflyingcar.warring_states.WarringStatesMod;
+import com.aflyingcar.warring_states.WarringStatesNetwork;
+import com.aflyingcar.warring_states.api.IWarGoal;
+import com.aflyingcar.warring_states.network.messages.WarDeclaredMessage;
 import com.aflyingcar.warring_states.states.State;
 import com.aflyingcar.warring_states.states.StateManager;
+import com.aflyingcar.warring_states.war.Conflict;
 import com.aflyingcar.warring_states.war.WarManager;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.logging.log4j.util.TriConsumer;
 
+import java.util.List;
 import java.util.Objects;
 
 @Mod.EventBusSubscriber(modid = WarringStatesMod.MOD_ID)
@@ -55,6 +64,13 @@ public class PlayerEventsHandler {
 
         if(state != null) {
             state.stopDecayTimer();
+
+            List<Conflict> conflicts = WarManager.getInstance().getAllConflictsInvolving(state);
+            callMethodForEveryConflict(event.player, state, conflicts, IWarGoal::onPlayerLogin);
+        }
+
+        for(Conflict war : WarManager.getInstance().getAllConflicts()) {
+            WarringStatesNetwork.NETWORK.sendTo(new WarDeclaredMessage(war), (EntityPlayerMP) event.player);
         }
     }
 
@@ -66,6 +82,30 @@ public class PlayerEventsHandler {
             if(state.getCitizens().stream().noneMatch(WarringStatesMod.proxy::isPlayerOnline)) {
                 state.startDecayTimer();
             }
+
+            List<Conflict> conflicts = WarManager.getInstance().getAllConflictsInvolving(state);
+            callMethodForEveryConflict(event.player, state, conflicts, IWarGoal::onPlayerLogout);
         }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerDeath(LivingDeathEvent event) {
+        EntityLivingBase entity = event.getEntityLiving();
+        if(!(entity instanceof EntityPlayer)) {
+            return;
+        }
+
+        State state = StateManager.getInstance().getStateFromPlayer((EntityPlayer)entity);
+
+        // This does not matter if the player is not a part of a state
+        if(state == null) return;
+
+        List<Conflict> conflicts = WarManager.getInstance().getAllConflictsInvolving(state);
+
+        callMethodForEveryConflict((EntityPlayer)entity, state, conflicts, IWarGoal::onPlayerDeath);
+    }
+
+    private static void callMethodForEveryConflict(EntityPlayer player, State state, List<Conflict> conflicts, TriConsumer<IWarGoal, EntityPlayer, Conflict.Side> consumer) {
+        conflicts.stream().map(c -> Pair.of(c.getWargoals(), c.getSideOf(state))).distinct().forEach(gss -> gss.getLeft().forEach(g -> consumer.accept(g, player, gss.getRight())));
     }
 }
