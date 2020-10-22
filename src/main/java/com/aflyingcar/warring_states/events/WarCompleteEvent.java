9b784ff9821aba2d69ff58ddd76aaebf85002364
Tyler Robbins <tyler@familyrobbins.com>
2020-10-21 20:37:20 -0700

    Added client-side-only versions of WarCompleteEvent and WarDeclaredEvent.

--- src/main/java/com/aflyingcar/warring_states/events/WarCompleteEvent.java
+++ src/main/java/com/aflyingcar/warring_states/events/WarCompleteEvent.java
@@ -1,6 +1,7 @@
 package com.aflyingcar.warring_states.events;
 
 import com.aflyingcar.warring_states.war.Conflict;
+import com.aflyingcar.warring_states.war.DummyConflict;
 import net.minecraftforge.fml.common.eventhandler.Event;
 
 public class WarCompleteEvent extends Event {
@@ -12,5 +13,17 @@ public class WarCompleteEvent extends Event {
 
     public Conflict getWar() {
         return war;
+    }
+
+    public static class CLIENT extends Event {
+        private DummyConflict war;
+
+        public CLIENT(DummyConflict war) {
+            this.war = war;
+        }
+
+        public DummyConflict getWar() {
+            return war;
+        }
     }
 }
